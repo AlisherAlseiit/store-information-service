2f4fea978d1ce2ec3cd4687c5e5c37607e7f0325
Alisher Alseiit <Your Name>
2021-09-25 19:24:56 +0600

    information microservice

---
+++ src/main/java/com/example/storeinformationservice/StoreInformationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.storeinformationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreInformationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreInformationServiceApplication.class, args);
+    }
+
+}
