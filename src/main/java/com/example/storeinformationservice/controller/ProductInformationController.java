1624195c88bc9961baf78cd04d3bc2b22d85f090
Alisher <Alisher>
2021-11-24 20:25:09 +0600

    kafka streaming

--- src/main/java/com/example/storeinformationservice/controller/ProductInformationController.java
+++ src/main/java/com/example/storeinformationservice/controller/ProductInformationController.java
@@ -4,6 +4,7 @@ import com.example.storeinformationservice.model.Product;
 import com.example.storeinformationservice.service.ProductInformationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +17,13 @@ import java.util.Optional;
 @RequestMapping("/products")
 public class ProductInformationController {
 
+    private static final String TOPIC = "store_test";
+
     @Autowired
     private ProductInformationService service;
+
+    @Autowired
+    private KafkaTemplate<String, Optional<Product>> kafkaTemplate;
 
     @GetMapping
     public List<Product> getProducts() {
@@ -26,6 +32,8 @@ public class ProductInformationController {
 
     @GetMapping("/{productId}")
     public Optional<Product> getAllProducts(@PathVariable("productId") Long id) {
+
+        kafkaTemplate.send(TOPIC, service.getProduct(id));
         return service.getProduct(id);
     }
 
